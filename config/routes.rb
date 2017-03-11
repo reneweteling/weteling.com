a694e25ef50fabfbb78796b0dd168ab04c1445ff
Rene Weteling <rene@weteling.com>
2017-03-11 13:24:45 +0100

    Running on cloud :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  get "_ah/health", to: "application#health_check"
+
+  root to: redirect('/admin')
 end
