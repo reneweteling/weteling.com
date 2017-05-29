c301e7db887a6ca7fe6e29a9cf532864b83201f5
Rene Weteling <rene@weteling.com>
2017-05-29 12:34:22 +0200

    Null

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -1,7 +1,7 @@
 class GraphController < ApplicationController
   # Use :null_session here
-  protect_from_forgery with: :null_session
-  skip_before_filter :verify_authenticity_token
+  # protect_from_forgery with: :null_session
+  # skip_before_filter :verify_authenticity_token
 
   def index
     render json: Schema.execute(GraphQL::Introspection::INTROSPECTION_QUERY)
