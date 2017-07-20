eb00bb3f12e97c6357de4c3ef6f0503c22fc1ae1
Rene Weteling <rene@weteling.com>
2017-07-20 08:27:27 +0200

    Set correct ability

--- app/models/abilities/admin_ability.rb
+++ app/models/abilities/admin_ability.rb
@@ -7,7 +7,7 @@ class AdminAbility
       can [:read, :update], User, id: user.id
       can :read, Project, id: user.projects.map(&:id)
       can :read, Client, id: user.projects.map(&:client_id)
-      can :read, ActiveAdmin::Page, name: "Dashboard"
+      can :manage, ActiveAdmin::Page, name: "Dashboard"
     end
   end
 end
