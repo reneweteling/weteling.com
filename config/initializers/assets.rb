7041b14571ba7f047dbb79b57db7f619fa255538
Rene Weteling <rene@weteling.com>
2017-09-13 11:34:53 +0200

    Cors

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,13 +1,19 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.configure do 
+  # Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+  # Version of your assets, change this if you want to expire all your assets.
+  config.assets.version = '1.0'
 
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
+  # Add additional assets to the asset load path
+  # Rails.application.config.assets.paths << Emoji.images_path
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+  # Precompile additional assets.
+  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+  # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( pdf.css )
+  config.assets.precompile += %w( pdf.css )
+
+  config.public_file_server.headers = {
+    'Access-Control-Allow-Origin' => '*'
+  }
+end
