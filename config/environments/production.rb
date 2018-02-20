9f1898ecefc288a713f9c8d8ac5ba18f7e51f2ff
Rene Weteling <rene@weteling.com>
2018-02-20 17:31:06 +0100

    Set the domain back

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.asset_host = 'https://weteling.dokku.weteling.com'
+  config.asset_host = 'https://www.weteling.com'
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
