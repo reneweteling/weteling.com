9f8bed4aebfbb3e585db4cf459fd09601c08a5df
Rene Weteling <rene@weteling.com>
2019-04-24 21:58:42 +0200

    Dokku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.asset_host = 'https://' + ENV['HOST'] || 'www.weteling.com'
+  config.asset_host = 'https://' + (ENV['HOST'] || 'www.weteling.com')
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: ENV['HOST'] || 'www.weteling.com', port: 443 }
+  config.action_mailer.default_url_options = { host: (ENV['HOST'] || 'www.weteling.com'), port: 443 }
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
