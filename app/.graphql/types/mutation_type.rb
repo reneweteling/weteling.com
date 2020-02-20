f3127eb91b5be2c7a2cdeb9ef27ddc116fc8ae1a
René Weteling <rene@weteling.com>
2020-02-20 07:54:01 +0000

    fix

--- app/.graphql/types/mutation_type.rb
+++
@@ -1,5 +0,0 @@
-MutationType = GraphQL::ObjectType.define do
-  name "Mutation"
-
-  # TODO: Add Mutations as fields
-end
--- app/.graphql/types/rate_type.rb
+++
@@ -1,8 +0,0 @@
- RateType = GraphQL::ObjectType.define do
-  name "Rate"
-  description "Type for Rate object"
-
-  field :description, GraphTypes::TEXT, "Property `description` for Rate"
-  field :name, GraphTypes::STRING, "Property `name` for Rate"
-  field :rate, GraphTypes::DECIMAL, "Property `rate` for Rate"
-end
