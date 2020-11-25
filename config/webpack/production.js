c2140f16dcd86f9474a2d3bcee73592f279119f3
René Weteling <rene@weteling.com>
2020-11-25 08:09:34 +0000

    wip

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,9 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+const environment = require('./environment')
+const config = environment.toWebpackConfig();
+delete(config.optimization.minimizer)
+
+module.exports = config
+
