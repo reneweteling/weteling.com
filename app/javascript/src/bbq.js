13398dce078becfe7431b0d051f48eaee811b296
Rene Weteling <rene@weteling.com>
2023-09-29 14:42:57 +0000

    chore: made it work

--- app/javascript/src/bbq.js
+++ app/javascript/src/bbq.js
@@ -1,82 +1,88 @@
-import { createConsumer } from "@rails/actioncable"
+import { createConsumer } from "@rails/actioncable";
 
-export default createConsumer()
+export default createConsumer();
 
 // Specify a different URL to connect to
-const consumer = createConsumer('/websocket')
+const consumer = createConsumer("/websocket");
 
-consumer.subscriptions.create({ channel: "BbqChannel"},
+consumer.subscriptions.create(
+  { channel: "BbqChannel" },
   {
-
     // Called when the subscription is ready for use on the server.
-  initialized() {
-    this.install()
-  },
+    initialized() {
+      this.install();
+    },
 
-  // Called when the WebSocket connection is closed.
-  disconnected() {
-    this.uninstall()
-  },
+    // Called when the WebSocket connection is closed.
+    disconnected() {
+      this.uninstall();
+    },
 
-  update(data) {
-    // Calls `AppearanceChannel#set(data)` on the server.
-    this.perform("update", data)
-  },
+    update(data) {
+      // Calls `AppearanceChannel#set(data)` on the server.
+      this.perform("update", data);
+    },
 
-  // Called when the subscription is rejected by the server.
-  rejected() {
-    this.uninstall()
-  },
+    // Called when the subscription is rejected by the server.
+    rejected() {
+      this.uninstall();
+    },
 
     received(data) {
       this.addConsole(data);
-      if (typeof data == "object" && data['state']){
-        this.updateState(data['state']);
+      if (typeof data == "object" && data["state"]) {
+        this.updateState(data["state"]);
       }
     },
 
     addConsole(data) {
-      if (typeof data == "object"){
-        data = JSON.stringify(data);
+      if (typeof data == "object") {
+        data = JSON.stringify(data.state);
       }
       this.output.innerText = this.output.innerText + "\n" + data;
       this.output.scrollTop = this.output.scrollHeight;
     },
 
-
-    updateState(state){
+    updateState(state) {
       this.state = {
         ...this.state,
-        ...state
-      }
+        ...state,
+      };
       for (const [key, value] of Object.entries(this.state)) {
         switch (key) {
-          case 'min':
+          case "min":
             document.getElementById("currentMin").innerText = value;
-          break;
-          case 'max':
+            break;
+          case "max":
             document.getElementById("currentMax").innerText = value;
-          break;
-          case 'temp':
+            break;
+          case "temp":
             document.getElementById("currentTemp").innerText = value;
-          break;
-          case 'target':
-            document.getElementById("currentTarget").value = value;
-          break;
-          case 'pos':
+            break;
+          case "targetTemp":
+            document.getElementById("targetTemp").value = value;
+            break;
+          case "pos":
             document.getElementById("currentPos").innerText = value;
-            if(this.state.min && this.state.max && this.state.min < this.state.max){
-              const delta = this.state.max - this.state.min
-              const pos = this.state.pos - this.state.min
-              this.state.percentage =  Math.round((pos / delta) * 100);
+            if (
+              this.state.min &&
+              this.state.max &&
+              this.state.min < this.state.max
+            ) {
+              const delta = this.state.max - this.state.min;
+              const pos = this.state.pos - this.state.min;
+              this.state.percentage = Math.round((pos / delta) * 100);
 
-              console.log([this.state, delta, pos])
+              console.log([this.state, delta, pos]);
 
-              document.getElementById("currentPercent").innerText = this.state.percentage;
-              document.getElementById("currentProgress").value = this.state.percentage;
-
+              document.getElementById(
+                "currentPercent"
+              ).innerText = `${this.state.percentage} %`;
+              document.getElementById(
+                "currentProgress"
+              ).value = this.state.percentage;
             }
-          break;
+            break;
         }
       }
     },
@@ -89,34 +95,48 @@ consumer.subscriptions.create({ channel: "BbqChannel"},
         targetTemp: 120,
         targetPos: 2000,
         min: 1000,
-        max:4000
+        max: 4000,
       });
 
-      document.getElementById("targetTemp").addEventListener("change", this.updateTarget.bind(this));
-      document.getElementById("close").addEventListener("click", this.clickclose.bind(this));
-      document.getElementById("open").addEventListener("click", this.clickopen.bind(this));
-      document.getElementById("min").addEventListener("click", this.clickMin.bind(this));
-      document.getElementById("max").addEventListener("click", this.clickMax.bind(this));
-    },
-  
-    uninstall() {
-      document.getElementById("close").removeEventListener("click", this.clickclose.bind(this));
+      document
+        .getElementById("targetTemp")
+        .addEventListener("change", this.updateTarget.bind(this));
+      document
+        .getElementById("close")
+        .addEventListener("click", this.clickclose.bind(this));
+      document
+        .getElementById("open")
+        .addEventListener("click", this.clickopen.bind(this));
+      document
+        .getElementById("min")
+        .addEventListener("click", this.clickMin.bind(this));
+      document
+        .getElementById("max")
+        .addEventListener("click", this.clickMax.bind(this));
     },
 
-    updateTarget(){
-      this.update({'targetTemp': document.getElementById("currentTarget").value})
+    uninstall() {
+      document
+        .getElementById("close")
+        .removeEventListener("click", this.clickclose.bind(this));
     },
-    clickclose(){
-      this.update({'targetPos': this.state.targetPos - 1000})
+
+    updateTarget() {
+      this.update({
+        targetTemp: document.getElementById("targetTemp").value,
+      });
     },
-    clickopen(){
-      this.update({'targetPos': this.state.targetPos + 1000})
+    clickclose() {
+      this.update({ targetPos: this.state.targetPos - 1000 });
     },
-    clickMin(){
-      this.update({'min': this.state.pos})
+    clickopen() {
+      this.update({ targetPos: this.state.targetPos + 1000 });
     },
-    clickMax(){
-      this.update({'max': this.state.pos})
+    clickMin() {
+      this.update({ min: this.state.pos });
+    },
+    clickMax() {
+      this.update({ max: this.state.pos });
     },
   }
-)
+);
