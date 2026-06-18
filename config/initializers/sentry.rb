57c2eb5f9974cd49562d5f3c3e3f2b58210bfa07
Rene Weteling <rene@weteling.com>
2026-06-18 09:37:03 +0000

    chore: point Sentry at hosted SaaS project DSN

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,8 @@
 Sentry.init do |config|
-  config.dsn = 'https://559fe397554f411d8b4b059c85548d15@glitchtip.weteling.com/4'
+  config.dsn = 'https://1a1767b8b1bbe998f0e6e630dcce3783@o157871.ingest.us.sentry.io/4511585502887936'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Add data like request headers and IP for users,
+  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+  config.send_default_pii = true
 end
